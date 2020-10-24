9d5aefa02a16139fcbc0e18b0cd1d42f2a709ea6
FelipeNunis <felipenunis@yahoo.com.br>
2020-10-24 16:27:11 -0300

    Atualização PISS-30, correção de construtor)

--- app/src/fatec/pi/controllers/SupplierController.java
+++ app/src/fatec/pi/controllers/SupplierController.java
@@ -7,8 +7,8 @@ import fatec.pi.entities.Supplier;
 import fatec.pi.views.ViewAccountType;
 
 public class SupplierController {
-	public static void saveValues(String cnpj, String name, String site, Integer Type) {
-		Supplier sup = new Supplier(cnpj, name, site);
+	public static void saveValues(String cnpj, String name, String site, Integer type) {
+		Supplier sup = new Supplier(cnpj, name, site, type);
 		if(SupplierDao.save(sup) == 1) {
 			showMessageDialog(null, "Dados cadastrados com Sucesso!");
 		}
