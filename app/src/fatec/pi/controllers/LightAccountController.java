2ccb74aad325ecf100c717e89c6c2868741acec4
Jeferson Neves <62185462+jef771@users.noreply.github.com>
2020-10-26 20:36:34 -0300

    PISS-24 (Controller conta de luz) PISS-25 (DAO da conta de luz) (#9)
    Inclusão do Controller e DAO da conta de luz.

---
+++ app/src/fatec/pi/controllers/LightAccountController.java
@@ -0,0 +1,26 @@
+package fatec.pi.controllers;
+
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class LightAccountController {
+	
+	public static void saveValues(String identCod, String meterNumber, String invoice, String currentDate,
+			String dueDate, Integer consumptionDays, String flagType, BigDecimal consumptionValue, BigDecimal pisPercentage,
+			BigDecimal cofinsPercentage, BigDecimal icmsBasis, BigDecimal icmsPercentage, BigDecimal icmsValue,
+			BigDecimal pisCofinsBasis, BigDecimal pisValue, BigDecimal cofinsValue, BigDecimal forfeitValue,
+			BigDecimal interestValue, BigDecimal otherValues, BigDecimal supplyValue, BigDecimal financialItems,
+			BigDecimal amount, String supplierCnpj, String clientCpf) {
+		
+		LightAccount light = new LightAccount(identCod, meterNumber, invoice, currentDate, dueDate, consumptionDays,
+				flagType, consumptionValue, pisPercentage, cofinsPercentage, icmsBasis, icmsPercentage, icmsValue,
+				pisCofinsBasis, pisValue, cofinsValue, forfeitValue, interestValue, otherValues, supplyValue,
+				financialItems, amount, supplierCnpj, clientCpf);
+		
+		if(LightAccountDao.save(light)) {
+			
+			showMessageDialog(null, "Dados cadastrados com Sucesso!");
+		}
+	}
+			
+
+}
