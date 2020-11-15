436ecc5d1935fcf7e360aaffb65f3b5ad761c0f0
Mike Barcelos <mikebbatista@hotmail.com>
2020-11-15 18:25:31 -0300

    classe do usuário

---
+++ app/src/fatec/pi/entities/User.java
@@ -0,0 +1,50 @@
+package fatec.pi.entities;
+
+public class User {
+	private String name;
+	private String email;
+	private String password;
+	private Boolean admin;
+	
+	public User(String name, String email, String password, Boolean admin) {
+		this.setName(name);
+		this.setEmail(email);
+		this.setPassword(password);
+		this.setAdmin(admin);
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(Boolean admin) {
+		this.admin = admin;
+	}
+	
+	
+}
